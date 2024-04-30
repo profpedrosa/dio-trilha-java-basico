313feba8eebb90b0066e9127cbe09ae5642ae876
profpedrosa <pedrosaprof@gmail.com>
2024-04-30 16:48:04 -0300

    projeto com métodos acrescentados

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,8 +2,26 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
-        System.out.printf("TV ligada: %b\n",smartTv.ligada);
+        System.out.printf("TV ligada? %b\n",smartTv.ligada);
         System.out.printf("Canal atual: %d\n",smartTv.canal);
         System.out.printf("Volume atual: %d\n",smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.printf("Volume atual: %d\n",smartTv.volume);
+
+        System.out.printf("Canal atual: %d\n",smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.printf("Canal atual: %d\n",smartTv.canal);
+
+        smartTv.ligar();
+        System.out.printf("Novo status -> TV ligada? %b\n",smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.printf("Novo status -> TV ligada? %b\n",smartTv.ligada);
+
+
     }
 }

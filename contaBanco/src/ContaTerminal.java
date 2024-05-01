22ab23f85869de47dfd0159e31a80b7ba4ae69da
profpedrosa <pedrosaprof@gmail.com>
2024-04-30 21:07:57 -0300

    Acrescentado comentários no código.

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
+        //importando a classe Scanner.
         Scanner sc = new Scanner(System.in);
 
+        //declaração da variável saldo armazenando seu valor.
         double saldo = 237.48;
 
+        //Exibindo mensagens solicitando informações ao usuário.
         System.out.print("Por favor, digite o número da sua conta: ");
         int numero = sc.nextInt();
         System.out.print("Por favor, digite o número da agência: ");
@@ -17,6 +20,8 @@ public class ContaTerminal {
         String nomeCliente= sc.nextLine();
            
         System.out.println("----------------------------------");
+
+        //exibindo as informações da conta criada.
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
 
         sc.close();
